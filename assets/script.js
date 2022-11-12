b1677fa7ffca8f5a4dba87fc623cb503cb2e069f
Sarahoo <sarahoo@Sarahoos-MacBook-Pro.local>
2022-11-13 00:57:07 +0330

    feature changed

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,23 @@ window.addEventListener("scroll", function () {
   var header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+// ------counter fun fact-----
+const counters = document.querySelectorAll(".counter");
+const speed = 200;
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const inc = target / speed;
+    console.log(inc);
+    if (count < target) {
+      counter.innerText = count + inc;
+      setTimeout(updateCount, 1);
+    } else {
+      count.innerText = target;
+    }
+  };
+  updateCount();
+});
