2dd35120fdad2ff1115048ac4113d7774ea19406
Sarahoo <sarahoo@Sarahoos-MacBook-Pro.local>
2022-11-12 19:32:45 +0330

    menuSticky in Progress

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,7 @@
-// window.addEventListener("scroll", function () {
-//   var header = document.querySelector("header");
-//   header.classList.toggle("sticky", (window.scrollY = 0));
-// });
+// let navBar = document.getElementById("navBar");
+// let manu = document.getElementById("manu");
+
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
